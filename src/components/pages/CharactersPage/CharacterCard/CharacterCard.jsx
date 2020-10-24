41411ea85c7252b88e6ba8e944e9b623b9bdabae
Rocio_Escobar <rociodelgadoesc@gmail.com>
2020-10-24 17:16:39 +0200

    characters fecthed!

---
+++ src/components/pages/CharactersPage/CharacterCard/CharacterCard.jsx
@@ -0,0 +1,20 @@
+// import React from 'react';
+
+export default function CharacterCard(props) {
+
+    //es el style del hijo card al padre characterpage
+    return (
+        <p>hi</p>
+    //     // <div className="row">
+    //     //     {/* function map - aplica las instrucciones que deseamos tener en la array */}
+    //     //     {​​​​​props.amiibos.map((amiibo, index) =>
+    //     //         <div className="col-4">
+    //     //             <figure>
+    //     //                 <p>{amiibo.name}​​​​​</p>
+    //     //                 <img src={amiibo.image} alt={amiibo.name}​​​​​></img>
+    //     //             </figure>
+    //     //         </div>
+    //     //     )}
+    //     // </div>
+    )
+}
--- src/components/pages/CharactersPage/CharactersPage.jsx
+++ src/components/pages/CharactersPage/CharactersPage.jsx
@@ -1,7 +1,62 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+//se extrae del paquete axios
+import axios from 'axios';
+
+//Nos va a permitir renderizar el componente hijo
+import CharacterCard from './CharacterCard/CharacterCard';
+
+//En esta variable guardará los datos del fecth
+let allCharacters = [];
 
 export default function CharactersPage() {
+    //Se estructura los Hooks 
+    //Use state - 
+
+    //Investigar--
+    const [characters, setCharacters] = useState([]);
+
+    useEffect(() => {
+        axios.get(process.env.REACT_APP_BACK_URL + 'characters/')
+        .then(res => {
+            //
+            allCharacters=res.data;
+            // console.log(allCharacters);
+            console.log(allCharacters[0].actor);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    } ,[] //Esto ayuda a que no haga repiticiones infinitas
+    )
+
     return (
-        <p>Soy los Characters</p>
+        <div>
+            <p>¿Qué Amiibo quieres encontrar?</p>
+            {/* Esto sirve para conectar al padre componenete con el hijo  */}
+            <CharacterCard characters={characters} />
+        </div>
     )
 }
+
+
+
+   // axios.get(process.env.REACT_APP_BACK_URL + 'characters/')
+        // .then(function (response) {
+        //     console.log(response);
+        // })
+        // .catch (function (error) {
+        //         console.log(error);
+        // })
+
+
+
+
+        //se genera el enlace a la API 
+        // axios.get(process.env.REACT_APP_BACK_URL + 'characters/')
+        //     //Se le indica lo que deseas obtener
+        //     .then(res => {
+        //         allCharacters = res.data;
+        //         console.log(allCharacters);
+        //         setCharacters(res.data);
+        //     })
+       
