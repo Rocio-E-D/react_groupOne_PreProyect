050ef3d83ff105d9d5f0bbe26dd6aed01585bfe4
CarlosAnegon <carlosanegon1@gmail.com>
2020-10-25 19:56:15 +0100

    Detail Page Connection ok2

--- src/components/pages/DetailPage/DetailPage.jsx
+++ src/components/pages/DetailPage/DetailPage.jsx
@@ -7,22 +7,24 @@ import { Link } from "react-router-dom";
 export default function DetailPage(props) {
     
     return (
-        <div className="row">
-        {/* Hacemos un map que nos aplique el mismo tratamiento a todos los caracteres de nuestro array */}
+
+      <p>holiii</p>
+      //   <div className="row">
+      //   {/* Hacemos un map que nos aplique el mismo tratamiento a todos los caracteres de nuestro array */}
   
-           {props.characters.map((character, index) => (
-            <figure className="col-6">
-            {/* esto pinta la carta con un parrafo nombre y la imagen */}
-                  <ul>
-                        <li>
-                             {character.name}
-                         </li>
-                {/* <li>
-                    <img src={character.image} alt={character.name} />{" "}
-                </li> */}
-                  </ul>
-          </figure>
-        ))}
-      </div>
+      //      {props.characters.map((character, index) => (
+      //       <figure className="col-6">
+      //       {/* esto pinta la carta con un parrafo nombre y la imagen */}
+      //             <ul>
+      //                   <li>
+      //                        {character.name}
+      //                    </li>
+      //           {/* <li>
+      //               <img src={character.image} alt={character.name} />{" "}
+      //           </li> */}
+      //             </ul>
+      //     </figure>
+      //   ))}
+      // </div>
     );
 }
