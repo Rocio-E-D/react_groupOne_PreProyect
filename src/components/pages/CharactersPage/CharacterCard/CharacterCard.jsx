0aa829448ce37073127e48140917c682e9474909
Sami Khalaf <samifalafel>
2020-10-25 12:35:26 +0100

    Game Of Throne fontimported and working!

--- src/components/pages/CharactersPage/CharacterCard/CharacterCard.jsx
+++ src/components/pages/CharactersPage/CharacterCard/CharacterCard.jsx
@@ -1,37 +1,25 @@
-import React from 'react';
-
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function CharacterCard(props) {
+  //es el style del hijo card al padre characterpage
+  return (
+    <div className="row">
+      {/* Hacemos un map que nos aplique el mismo tratamiento a todos los caracteres de nuestro array */}
 
-    //es el style del hijo card al padre characterpage
-    return (
-       
+      {props.characters.map((character, index) => (
+        <div className="col-4">
+          <figure>
+            {/* esto pinta la carta con un parrafo nombre y la imagen */}
 
-        <div className="row">
-          {/* Hacemos un map que nos aplique el mismo tratamiento a todos los caracteres de nuestro array */}
-            {props.characters.map((character,index)=>
-            <div className="col-4">
-                <figure>
-                    {/* esto pinta la carta con un parrafo nombre y la imagen */}
-                    <p>
-                        {character.name}
-                    </p>
-                    <img src={character.image} alt={character.name}/>
-                </figure>
-            </div>
-                
-            )}
+            <Link to={`${character.name}`}> {character.name} </Link>
+            <Link to={`${character.name}`}>
+              {" "}
+              <img src={character.image} alt={character.name} />{" "}
+            </Link>
+          </figure>
         </div>
-        // <div className="row">
-        //     {/* function map - aplica las instrucciones que deseamos tener en la array */}
-        //     {​​​​​props.map((character, index) =>
-        //         <div className="col-4">
-        //             <figure>
-        //                 <p>{character.name}​​​​​</p>
-        //                 <img src={character.image} alt={character.name}​​​​​>
-        //             </figure>
-        //         </div>
-        //     )}
-        // </div>
-    )
+      ))}
+    </div>
+  );
 }
